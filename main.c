ad1b8ce469514e0344ebbbb123f5c5343563a057
pross1312 <dangvinhtuong12@gmail.com>
2024-10-11 07:37:08 +0700

    fix buzzing noise when play multiple notes
    more complex fade system, which change max_amplitude
    of each notes to keep total amplitude between [0, MAX_AMPLITUDE]

--- main.c
+++ main.c
@@ -8,85 +8,111 @@
 #define SAMPLE_RATE 44100
 #define NOTE_INC 1.0594630943592953f // 2^12
 #define BASE_FREQUENCY 261.63f
-#define MAX_AMPLITUDE 5000.0f
+#define MAX_AMPLITUDE 12000.0f
 #define FADE_TIME 0.15f // second
-#define FADE_SAMPLE (SAMPLE_RATE*FADE_TIME)
-#define AMP_PER_SAMPLE (MAX_AMPLITUDE/FADE_SAMPLE)
+#define FADE_SAMPLE_COUNT (SAMPLE_RATE*FADE_TIME)
+#define AMP_PER_SAMPLE (MAX_AMPLITUDE/FADE_SAMPLE_COUNT)
 
 typedef enum {
     PHASE_STOP = 0,
     PHASE_FADE_IN,
-    PHASE_PLAYING,
     PHASE_FADE_OUT,
+    PHASE_FADE_DOWN,
+    PHASE_FADE_UP,
+    PHASE_PLAYING,
 } Phase;
 
 typedef struct {
     float frequency;
     float sineidx;
     float amplitude;
-    uint32_t phase_sample_played;
+    float max_amplitude;
     Phase phase;
 } Note;
 
-#define NOTES_COUNT 12
+#define NOTES_COUNT 13
 Note notes[NOTES_COUNT] = {0};
+size_t active_notes = 0;
 const char note_keys[NOTES_COUNT] = {
-    'Q', '2', 'W', '3', 'E', 'R', '5', 'T', '6', 'Y', '7', 'U'
+    'Q', '2', 'W', '3', 'E', 'R', '5', 'T', '6', 'Y', '7', 'U', 'I'
 };
 
+
 void start_note(Note *note) {
+    active_notes += 1;
     note->phase = PHASE_FADE_IN;
-    note->phase_sample_played = 0;
-    note->amplitude = 0;
-    note->sineidx = 0;
 }
 void stop_note(Note *note) {
-    if (note->phase == PHASE_PLAYING) {
-        note->phase = PHASE_FADE_OUT;
-        note->phase_sample_played = 0;
-    } else if (note->phase == PHASE_FADE_IN) {
-        note->phase = PHASE_FADE_OUT;
-        note->phase_sample_played = FADE_SAMPLE - note->phase_sample_played;
-    } else {
-        assert(false && "Invalid pre phase into fade out");
+    active_notes -= 1;
+    note->phase = PHASE_FADE_OUT;
+}
+void set_note_max_amplitude(Note *note, float new_max_amplitude) {
+    if (note->amplitude > new_max_amplitude) {
+        note->phase = PHASE_FADE_DOWN;
+    } else if (note->amplitude < new_max_amplitude) {
+        note->phase = PHASE_FADE_UP;
     }
+    note->max_amplitude = new_max_amplitude;
 }
 
 // 44100 sample/second
 float get_note_amplitude(Note *note) {
-
     float result = 0;
     switch (note->phase) {
     case PHASE_STOP:
         return 0;
         break;
     case PHASE_FADE_IN:
-        if (note->phase_sample_played >= FADE_SAMPLE) {
+        if (note->amplitude >= note->max_amplitude) {
             note->phase = PHASE_PLAYING;
-            note->phase_sample_played = 0;
-            note->amplitude += AMP_PER_SAMPLE;
+            note->amplitude = note->max_amplitude;
         } else {
-            note->amplitude = note->phase_sample_played/FADE_SAMPLE * MAX_AMPLITUDE;
+            note->amplitude += note->max_amplitude/FADE_SAMPLE_COUNT;
+        }
+        break;
+    case PHASE_FADE_OUT:
+        if (note->amplitude <= 0) {
+            note->phase = PHASE_STOP;
+            note->amplitude = 0;
+            note->sineidx = 0;
+        } else {
+            note->amplitude -= note->max_amplitude/FADE_SAMPLE_COUNT;
+        }
+        break;
+    case PHASE_FADE_UP:
+        if (note->amplitude >= note->max_amplitude) {
+            note->phase = PHASE_PLAYING;
+            note->amplitude = note->max_amplitude;
+        } else {
+            if (active_notes == 1) {
+                note->amplitude += note->max_amplitude/FADE_SAMPLE_COUNT;
+            } else {
+                note->amplitude += (note->max_amplitude/FADE_SAMPLE_COUNT)/(active_notes-1);
+            }
+        }
+        break;
+    case PHASE_FADE_DOWN:
+        if (note->amplitude <= note->max_amplitude) {
+            note->phase = PHASE_PLAYING;
+            note->amplitude = note->max_amplitude;
+        } else {
+            if (active_notes == 1) {
+                note->amplitude -= note->max_amplitude/FADE_SAMPLE_COUNT;
+            } else {
+                note->amplitude -= (note->max_amplitude/FADE_SAMPLE_COUNT)/(active_notes-1);
+            }
         }
         break;
     case PHASE_PLAYING:
-        note->amplitude = MAX_AMPLITUDE;
-        break;
-    case PHASE_FADE_OUT:
-        if (note->phase_sample_played >= FADE_SAMPLE) {
-            note->phase = PHASE_STOP;
-            note->phase_sample_played = 0;
-            note->phase_sample_played = 0;
-        } else {
-            note->amplitude -= AMP_PER_SAMPLE;
-        }
+        note->amplitude = note->max_amplitude;
         break;
     }
 
+    // result = note->amplitude * (2.0f * fabsf(2.0f * note->sineidx - 1.0f) - 1.0f);
+    // result = note->amplitude * (note->sineidx < 0.5f ? 1.0f : -1.0f);
     result = note->amplitude * sinf(2*PI*note->sineidx);
     note->sineidx += note->frequency/SAMPLE_RATE;
-    if (note->sineidx > 1.0f) note->sineidx -= 1.0f;
-    note->phase_sample_played += 1;
+    if (note->sineidx >= 1.0f) note->sineidx -= 1.0f;
     return result;
 }
 
@@ -94,17 +120,20 @@ void AudioInputCallback(void *buffer, unsigned int samples)
 {
     int16_t *d = (int16_t *)buffer;
     for (unsigned int i = 0; i < samples; i++) {
-        int16_t amplitude = 0.0f;
+        float amplitude = 0.0f;
         for (size_t i = 0; i < NOTES_COUNT; i++) {
-            amplitude += (int16_t)get_note_amplitude(&notes[i]);
+            if (notes[i].phase != PHASE_STOP) {
+                amplitude += get_note_amplitude(&notes[i]);
+            }
         }
-        d[2*i] = d[2*i+1] = amplitude;
+        d[i] = (int16_t)amplitude;
     }
 }
 
 int main() {
     int factor = 100;
     InitWindow(16*factor, 9*factor, "Testing sound");
+    SetTargetFPS(60);
     InitAudioDevice();
 
     float fre = BASE_FREQUENCY;
@@ -114,7 +143,7 @@ int main() {
     }
 
     SetAudioStreamBufferSizeDefault(MAX_SAMPLES_PER_UPDATE);
-    AudioStream stream = LoadAudioStream(SAMPLE_RATE, 16, 2);
+    AudioStream stream = LoadAudioStream(SAMPLE_RATE, 16, 1);
     SetAudioStreamCallback(stream, AudioInputCallback);
 
     PlayAudioStream(stream);
@@ -124,8 +153,20 @@ int main() {
         for (size_t i = 0; i < NOTES_COUNT; i++) {
             if (IsKeyPressed(note_keys[i])) {
                 start_note(&notes[i]);
+                notes[i].max_amplitude = MAX_AMPLITUDE/fmax(active_notes, 1);
+                for (size_t j = 0; j < NOTES_COUNT; j++) {
+                    if (i != j && notes[j].phase != PHASE_STOP && notes[j].phase != PHASE_FADE_OUT) {
+                        set_note_max_amplitude(&notes[j], MAX_AMPLITUDE/fmax(active_notes, 1));
+                    }
+                }
             } else if (IsKeyReleased(note_keys[i])) {
                 stop_note(&notes[i]);
+                notes[i].max_amplitude = MAX_AMPLITUDE/fmax(active_notes, 1);
+                for (size_t j = 0; j < NOTES_COUNT; j++) {
+                    if (i != j && notes[j].phase != PHASE_STOP && notes[j].phase != PHASE_FADE_OUT) {
+                        set_note_max_amplitude(&notes[j], MAX_AMPLITUDE/fmax(active_notes, 1));
+                    }
+                }
             }
         }
         EndDrawing();
